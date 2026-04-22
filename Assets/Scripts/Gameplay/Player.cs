b42f82cbfcac544fcb9e8d3c2d5df9bb16c729b7
Tick <62646477+Tick-git@users.noreply.github.com>
2026-04-23 00:43:09 +0200

    Adds Basic Movement

--- Assets/Scripts/Gameplay/Player.cs
+++ Assets/Scripts/Gameplay/Player.cs
@@ -6,18 +6,35 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private AssaultRifle _assaultRifle;
     [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private float _speed = 3f;
+    [SerializeField] private Animator _animator;
     
     private Camera _cam;
+    private InputAction _moveAction;
 
     private void Awake()
     {
         _cam = Camera.main;
+        _moveAction = InputSystem.actions.FindAction("Move");
     }
 
     private void Update()
     {
         Vector3 target = _cam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         target.z = 0;
+
+        Vector3 inputVector = _moveAction.ReadValue<Vector2>();
+        
+        transform.position += inputVector * (Time.deltaTime * _speed);
+
+        if (inputVector != Vector3.zero)
+        {
+            _animator.Play("Walk");
+        }
+        else
+        {
+            _animator.Play("Idle");
+        }
         
         _assaultRifle.ApplyAim(target);
         
