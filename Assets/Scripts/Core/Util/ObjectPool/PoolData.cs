e5d5f4511e65fb0b4abaa1ce30ae000d77893b0e
Tick <62646477+Tick-git@users.noreply.github.com>
2026-04-23 12:20:16 +0200

    Adds Object Pool Data Scriptable Object

---
+++ Assets/Scripts/Core/Util/ObjectPool/PoolData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/Util/Pool")]
+public class PoolData : ScriptableObject
+{
+    [SerializeField] private GameObject _prefab;
+    [SerializeField] private int _poolSize;
+    
+    public GameObject Prefab => _prefab;
+
+    public int PoolSize => _poolSize;
+}
