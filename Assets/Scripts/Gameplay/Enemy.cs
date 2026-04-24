0cadf72a59277a3fa743e1c47b6fd24c12861c62
Tick <62646477+Tick-git@users.noreply.github.com>
2026-04-24 11:36:59 +0200

    Improves Health Component

--- Assets/Scripts/Gameplay/Enemy.cs
+++ Assets/Scripts/Gameplay/Enemy.cs
@@ -4,6 +4,7 @@ public class Enemy : MonoBehaviour
 {
     private void Awake()
     {
-        GetComponent<Health>().OnHealthChanged += (x) => Debug.Log(x);
+        GetComponent<Health>().HealthChanged += (x) => Debug.Log(x);
+        GetComponent<Health>().Died += () => Debug.Log("ENEMY DEAD");
     }
 }
--- Assets/Scripts/Gameplay/Systems/Health.cs
+++ Assets/Scripts/Gameplay/Systems/Health.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 public class Health : MonoBehaviour, IDamageable
 {
     private readonly float _maxHealth = 100f;
+    private readonly float _minHealth = 0f;
+    
     private float _currentHealth;
     
-    public event Action<float> OnHealthChanged;
-    public event Action OnDeath;
+    public event Action<float> HealthChanged;
+    public event Action Died;
 
     private void Awake()
     {
@@ -16,11 +18,16 @@ public class Health : MonoBehaviour, IDamageable
 
     public void ApplyDamage(float damage)
     {
+        if (_currentHealth <= _minHealth) return;
+        
         _currentHealth -= damage;
 
-        if (_currentHealth <= 0)
-            OnDeath?.Invoke();
-        else
-            OnHealthChanged?.Invoke(_currentHealth);
+        if (_currentHealth <= _minHealth)
+        {
+            _currentHealth = _minHealth;
+            Died?.Invoke();
+        }
+
+        HealthChanged?.Invoke(_currentHealth);
     }
 }
