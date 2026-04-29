61e97119b8512d3b7d310bfe504e07031c98e21d
Tick <62646477+Tick-git@users.noreply.github.com>
2026-04-29 20:46:27 +0200

    Saves Boss States In Separate Files

---
+++ Assets/Scripts/Gameplay/Boss/StateMachine/AttackState.cs
@@ -0,0 +1,59 @@
+﻿namespace Gameplay.Boss
+{
+    public class AttackState : BaseState<BossController>
+    {
+        private State _currentState; 
+        
+        enum State
+        {
+            None,
+            Aim,
+            Shoot,
+            Holster
+        }
+        
+        public AttackState(BossController context) : base(context) { }
+
+        public override void Enter() => _currentState = State.None;
+
+        public override void Exit()
+        {
+            _currentState = State.None;
+        }
+
+        public override void Update()
+        {
+            TryChangeState();
+            
+            Context.Weapon.ApplyAim(Context.TargetTracker.GetTargetPosition());
+            
+            switch (_currentState)
+            {
+                case State.Aim:
+                    break;
+                case State.Shoot:
+                    break;
+                case State.Holster:
+                    break;
+            }
+        }
+
+        private void TryChangeState()
+        {
+            if (_currentState == State.None)
+            {
+                _currentState = State.Aim;
+                Context.Animator.SetAimTrigger();
+            }
+            else if (_currentState == State.Aim && !Context.Animator.AimingRunning)
+            {
+                _currentState = State.Shoot;
+                Context.Weapon.Shoot(Context.TargetTracker.GetTargetPosition());
+            } 
+            else if (_currentState == State.Shoot && !Context.Animator.ShootRunning)
+            {
+                _currentState = State.Holster;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/Boss/StateMachine/IdleState.cs
@@ -0,0 +1,7 @@
+﻿namespace Gameplay.Boss
+{
+    public class IdleState : BaseState<BossController>
+    {
+        public IdleState(BossController context) : base(context) { }
+    }
+}
