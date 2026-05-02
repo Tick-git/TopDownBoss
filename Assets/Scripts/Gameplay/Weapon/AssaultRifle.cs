b8929922335c81a150e81d8b9e8f250c99b44d63
Tick <62646477+Tick-git@users.noreply.github.com>
2026-05-02 11:49:34 +0200

    Adds Player Aim Smoothing

--- Assets/Scripts/Gameplay/Weapon/AssaultRifle.cs
+++ Assets/Scripts/Gameplay/Weapon/AssaultRifle.cs
@@ -8,12 +8,14 @@ public class AssaultRifle : MonoBehaviour
     [SerializeField] private Magazine _magazine;
     
     private float _lastShotTime;
-    private WeaponRotator _weaponRotator;
+    private Vector2 _currentDirection;
+    SpriteRenderer _spriteRenderer;
     private Vector2 CenterPosition => _weaponCenter.position;
     
+    private const float DirectionSmoothFactor = 10;
     public void Initialize()
     {
-        _weaponRotator = new WeaponRotator(GetComponent<SpriteRenderer>(), transform);
+        _spriteRenderer = GetComponent<SpriteRenderer>();
         _lastShotTime = -float.MaxValue;
         
         ApplyAim(Vector2.right);
@@ -23,9 +25,30 @@ public class AssaultRifle : MonoBehaviour
     {
         if (direction.magnitude <= 0.001f)
             return;
+        
+        _currentDirection = SmoothDirection(direction.normalized, Time.deltaTime);
 
-        _weaponRotator.Rotate(direction);
-        transform.position = CenterPosition + direction * _data.OrbitRadius;
+        bool aimingLeft = direction.x < 0;
+        float rotationAngle = Mathf.Atan2(_currentDirection.y, _currentDirection.x) * Mathf.Rad2Deg;
+        
+        _spriteRenderer.flipY = aimingLeft;
+        transform.rotation = Quaternion.Euler(0, 0, rotationAngle);
+        transform.position = CenterPosition + _currentDirection * _data.OrbitRadius;
+    }
+
+    private Vector2 SmoothDirection(Vector2 direction, float deltaTime)
+    {
+        Vector2 result = direction;
+        
+        float dot = Vector2.Dot(_currentDirection.normalized, direction);
+        bool smallDirectionDifference = dot > 0;
+        
+        if (smallDirectionDifference)
+        {
+            result = Vector2.Lerp(_currentDirection, direction, deltaTime * (DirectionSmoothFactor / dot)) ;
+        }
+
+        return result;
     }
 
     public void TryShoot()
