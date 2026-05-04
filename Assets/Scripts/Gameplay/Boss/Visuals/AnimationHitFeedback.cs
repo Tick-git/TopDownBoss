e6a5491eef53dcc2c85691498a5e3f4abcfbb239
Tick <62646477+Tick-git@users.noreply.github.com>
2026-05-04 18:00:24 +0200

    Adds Hit Animation To Boss

---
+++ Assets/Scripts/Gameplay/Boss/Visuals/AnimationHitFeedback.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+public class AnimationHitFeedback : MonoBehaviour
+{
+    [SerializeField] Animator _animator;
+    [SerializeField] Health _health;
+
+    private static readonly int Hit = Animator.StringToHash("Hit");
+    
+    private void OnEnable()
+    {
+        _health.HealthChanged += OnHealthChanged;
+    }
+
+    private void OnDisable()
+    {
+        _health.HealthChanged -= OnHealthChanged;
+    }
+
+    private void OnHealthChanged(float obj)
+    {
+        _animator.SetTrigger(Hit);
+    }
+}
