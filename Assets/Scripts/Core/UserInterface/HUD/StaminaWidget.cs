a9519d16e562a8ee0e4e507559f8843030e610c2
Tick <62646477+Tick-git@users.noreply.github.com>
2026-05-08 21:41:50 +0200

    Creates Stamina Widget Class

---
+++ Assets/Scripts/Core/UserInterface/HUD/StaminaWidget.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.UIElements;
+
+namespace Core.UserInterface.HUD
+{
+    public class StaminaWidget : HUDWidget
+    {
+        private readonly Slider _staminaBar;
+        
+        public StaminaWidget(VisualElement root, string barRootName, float maxStamina) : base(root)
+        {
+            _staminaBar = root.Q(barRootName).Q<Slider>("FillableBar");
+            _staminaBar.highValue = maxStamina;
+        }
+
+        public void SetStamina(float value)
+        {
+            _staminaBar.value = value;
+        }
+    }
+}
