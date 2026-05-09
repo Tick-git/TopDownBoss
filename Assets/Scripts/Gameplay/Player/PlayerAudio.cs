afba6eea9f6fd5a6ae30d81d3a6ed1a9f50b40a6
Tick <62646477+Tick-git@users.noreply.github.com>
2026-05-09 11:31:13 +0200

    Creates Player Audio Script

---
+++ Assets/Scripts/Gameplay/Player/PlayerAudio.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace Gameplay.Player
+{
+    public class PlayerAudio : MonoBehaviour
+    {
+        [SerializeField] private AudioData _shotSound;
+        [SerializeField] private AudioData _rollSound;
+        [SerializeField] private AudioData _stepSound;
+
+        private AudioManager _audioManager;
+        
+        private void Awake()
+        {
+            _audioManager = AudioManager.Instance;
+        }
+        
+        public void PlayShootSfx() => Play(_shotSound);
+
+        public void PlayRollSfx() => Play(_rollSound);
+        
+        public void PlayStepSfx() => Play(_stepSound);
+
+        private void Play(AudioData data)
+        {
+            if (_audioManager == null) return;
+            
+            _audioManager.PlaySfx(_shotSound);
+        }
+    }
+}
