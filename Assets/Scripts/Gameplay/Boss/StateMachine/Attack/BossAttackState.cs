a8fec6f056c40b93031cbf3d43586ade33c64b93
Tick <62646477+Tick-git@users.noreply.github.com>
2026-05-21 12:49:41 +0200

    Creates Base Class For Boss Attack States

---
+++ Assets/Scripts/Gameplay/Boss/StateMachine/Attack/BossAttackState.cs
@@ -0,0 +1,55 @@
+using Gameplay.Boss;
+
+public abstract class BossAttackState : IState
+{
+    protected BossController Context;
+    public bool IsRunning { get; private set; }
+
+    protected BossAttackState(BossController context)
+    {
+        Context = context;
+    }
+    
+    public virtual void Enter()
+    {
+        IsRunning = true;
+        Context.AttackDecider.NotifyAttackStarted();
+        Context.AttackSequenceRunner.AnimationChanged += OnAnimationChanged;
+        Context.AttackSequenceRunner.SequenceFinished += OnSequenceFinished;
+    }
+
+    protected virtual void OnSequenceFinished()
+    {
+        CleanUp();
+    }
+
+    protected virtual void OnAnimationChanged(AttackAnimationType animationType)
+    {
+        // NOOP
+    }
+
+
+    public virtual void Update()
+    {
+        // NOOP
+    }
+
+    public virtual void FixedUpdate()
+    {
+        // NOOP
+    }
+
+    public virtual void Exit()
+    {
+        CleanUp();
+        Context.AttackDecider.NotifyAttackEnded();
+        Context.Animator.PlayIdle();
+    }
+
+    private void CleanUp()
+    {
+        IsRunning = false;
+        Context.AttackSequenceRunner.AnimationChanged -= OnAnimationChanged;
+        Context.AttackSequenceRunner.SequenceFinished -= OnSequenceFinished;
+    }
+}
